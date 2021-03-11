b62ad62087d9912621bf2470171169ac4a4a9078
saralpowers <63485240+saralpowers@users.noreply.github.com>
2021-03-10 22:54:46 -0800

    first change

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -6,6 +6,22 @@
 // IMPORT THE MODULE
 
 // COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
+let num1 = prompt('Enter the first number: ');
+let num2 = prompt('Enter the second number: ');
+let operation = prompt('Enter the operation you want to perform (add, subtract, multiply, or divide): ');
+
 
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
 // CALL THE APPROPRIATE FUNCTION
+
+import * as calc from "../calculator.js";
+
+if (operation === "add") {
+    console.log(calc.add(num1, num2));
+} else if (operation === "subtract") {
+    console.log(calc.subtract(num1, num2));
+} else if (operation === "multiply") {
+    console.log(calc.multiply(num1, num2));
+} else (operation === "divide") {
+    console.log(calc.divide(num1, num2));
+};
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,54 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
+//private by default
+
+function calculate(num1, num2) {
+//switch statement to evaluate operation parameter that calls function below
+switch (operation) {
+    case "add":
+        const add(num1, num2)
+        break;
+    case "subtract":
+        function subtract(num1, num2) {};
+        break; 
+    case "multiply":
+        function multiply(num1, num2) {};
+        break; 
+    case "divide":
+        function divide(num1, num2) {};
+        break; 
+    default:
+        document.write('Invalid entry.');
+}
+}
 
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 
 // ADD FUNCTION
 
+const add = (x, y) => {
+    return x + y;
+}
+export {add};
+
 // SUBTRACT FUNCTION
+
+const subtract = (x, y) => {
+    return x - y;
+}
+export {subtract};
 
 // MULTIPLY FUNCTION
 
+const multiply = (x, y) => {
+    return x * y;
+}
+export {multiply};
+
 // DIVIDE FUNCTION
+
+const divide = (x, y) => {
+    return x / y;
+}
+export {divide};
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
